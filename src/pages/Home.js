7dff7c50a61ae5602dce31b6f9c912317646888b
harunLGS <Harun@ddptech.com.au>
2025-12-12 01:43:33 +0530

    made all the sections of homepage responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,6 @@ export default function Home() {
       <WhyChoose />
       <OurValues />
       <OurWorkProcess />
-      {/* <Testimonials /> */}
       <OurPartner />
       <ConsultationForm />
     </>
